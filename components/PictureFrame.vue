833f483fc5c37bafc3741760da7f45672d757140
Dan Lourenco <danlourenco@gmail.com>
2024-04-07 16:42:42 -0400

    style cleanup

--- components/PictureFrame.vue
+++ components/PictureFrame.vue
@@ -3,43 +3,35 @@ export interface Props {
   src: string;
 }
 
-// const getImage = (imgFile: string) => {
-//   // const url = new URL(`../assets/artwork/${imgFile}`, import.meta.url).href;
-//     const url = new URL(`${imgFile}`, import.meta.url).href;
-//   console.log(url);
-//   return url;
-// }
-
 const props = withDefaults(defineProps<Props>(), {
   src: "https://placehold.co/600x400",
 });
-
-// const props = defineProps<Props>();
-
 </script>
 <template>
-  <div class="m-16">
+  <!-- <div class="m-16"> -->
+  <div class="container">
     <div class="frame">
       <div class="mat">
-        <div class="art">
-          <img :src="props.src" alt="Rebellion" />
+        <div class="art overflow-hidden">
+          <img :src="props.src" alt="Rebellion" class="object-cover" />
         </div>
       </div>
     </div>
   </div>
+
+  <!-- </div> -->
 </template>
 
 <style scoped>
 .frame {
-  min-width: 300px;
-  max-width: 450px;
+  aspect-ratio: 4 / 3;
   position: relative;
   width: 100%;
-  padding-bottom: 82.5%;
   background: black;
-  /* box-shadow: 0 10px 7px -5px rgba(0, 0, 0, 0.3); */
-  box-shadow: inset 1px 1px 3px rgba(0, 0, 0, 0.3),
-    1px 1px 2px rgba(0, 0, 0, 0.2), 40px 35px 70px -10px rgba(0, 0, 0, 0.5),
+  box-shadow:
+    inset 1px 1px 3px rgba(0, 0, 0, 0.3),
+    1px 1px 2px rgba(0, 0, 0, 0.2),
+    40px 35px 70px -10px rgba(0, 0, 0, 0.5),
     -20px -20px 100px -10px rgba(255, 255, 255, 0.8);
 }
 .mat {
